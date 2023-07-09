20ed7d32438524b6e5b6780c71c556e24a30a317
Muhammad Fikri <fikri2347@gmail.com>
2023-07-09 11:02:53 +0700

    Initial App

---
+++ app.js
@@ -0,0 +1,22 @@
+const qrcode = require('qrcode-terminal');
+
+const { Client } = require('whatsapp-web.js');
+const client = new Client();
+
+client.on('qr', qr => {
+    qrcode.generate(qr, {small: true});
+});
+
+client.on('ready', () => {
+    console.log('Client is ready!');
+});
+
+client.on('message', message => {
+    console.log(message.body);
+    
+    if(message.body === '!hello') {
+      message.reply('Hello!');
+    }
+});
+
+client.initialize();
